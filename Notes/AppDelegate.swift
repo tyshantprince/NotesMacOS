2dbb84221376ad73bcc3df50168eeff255da3e77
Asia Dior <newmac@asiadior.local>
2018-04-26 17:14:10 -0500

    Initial Commit

---
+++ Notes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Notes
+//
+//  Created by Asia Dior on 4/26/18.
+//  Copyright © 2018 Tyshan Prince. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
